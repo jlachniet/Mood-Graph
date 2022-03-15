11ec31542267a9576b092b8ed8aea9c4d004b08e
Julian Lachniet <julian@lachniet.com>
2022-03-15 16:12:51 -0400

    Add auto-scroll to the bottom

--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -2,12 +2,19 @@ import { Pixel, PixelSelector } from '../../types/pixels';
 import { ClientUserSettings } from '../../types/settings';
 import { pixelsToPixelYear } from '../../utils/pixels';
 import { CalendarYear } from './CalendarYear';
+import { useEffect, useRef } from 'react';
 
 export function Calendar(props: {
 	pixels: Pixel[];
 	settings: ClientUserSettings;
 	setSelectedPixel: PixelSelector;
 }) {
+	const bottomRef = useRef<HTMLLIElement>(null);
+
+	useEffect(() => {
+		bottomRef.current?.scrollIntoView();
+	}, []);
+
 	const pixelYears = pixelsToPixelYear(
 		props.pixels,
 		props.settings.startDate,
@@ -15,14 +22,17 @@ export function Calendar(props: {
 	);
 
 	return (
-		<ul className="max-h-full overflow-auto [scrollbar-gutter:stable_both-edges]">
-			{pixelYears.map((pixelYear) => (
-				<CalendarYear
-					key={pixelYear.year}
-					pixelYear={pixelYear}
-					setSelectedPixel={props.setSelectedPixel}
-				/>
-			))}
-		</ul>
+		<>
+			<ul className="max-h-full overflow-auto [scrollbar-gutter:stable_both-edges]">
+				{pixelYears.map((pixelYear) => (
+					<CalendarYear
+						key={pixelYear.year}
+						pixelYear={pixelYear}
+						setSelectedPixel={props.setSelectedPixel}
+					/>
+				))}
+				<li ref={bottomRef} />
+			</ul>
+		</>
 	);
 }
