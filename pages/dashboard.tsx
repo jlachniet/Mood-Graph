ec35a3cdb579758f5b7a10aac93962f4cd9b476f
Julian Lachniet <julian@lachniet.com>
2022-03-15 15:33:24 -0400

    Switch the overlay to fixed, should fix the bug

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -41,7 +41,7 @@ export default function Dashboard() {
 						/>
 					)}
 					<div
-						className="absolute inset-0 z-30 cursor-pointer bg-neutral-900 opacity-50"
+						className="fixed inset-0 z-30 cursor-pointer bg-neutral-900 opacity-50"
 						onClick={() => {
 							setSelectedPixel(null);
 							setSettingsMenuState('closed');
