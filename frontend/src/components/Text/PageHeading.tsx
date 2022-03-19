3475faeb779e04d0502b283a44c1b3a60c7a03b1
Julian Lachniet <julian@lachniet.com>
2022-03-19 16:29:32 -0400

    FE: Remove an unnecessary variable declaration

--- frontend/src/components/Text/PageHeading.tsx
+++ frontend/src/components/Text/PageHeading.tsx
@@ -1,7 +1,8 @@
 import { mergeElementProps } from '../../utils/components';
 
 export function PageHeading(props: JSX.IntrinsicElements['h1']) {
-	const template = <h1 className="font-display text-3xl font-extrabold" />;
-
-	return mergeElementProps(template, props);
+	return mergeElementProps(
+		<h1 className="font-display text-3xl font-extrabold" />,
+		props
+	);
 }
